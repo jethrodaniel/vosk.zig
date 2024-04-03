22258f62d885d21babf2665a4b419876d453a4c0
Mark Delk <jethrodaniel@gmail.com>
2024-04-03 17:17:05 -0500

    update release script

--- release.sh
+++ release.sh
@@ -12,18 +12,20 @@ tree tmp
 cd tmp
 
 # NOTE: on linux, use --owner=0 --group=0
-tar czvf vosk-0.3.45-x86_64-macos.tar.gz --uid=0 --gid=0 x86_64-macos
-tar czvf vosk-0.3.45-aarch64-macos.tar.gz --uid=0 --gid=0 aarch64-macos
+tar czvf vosk-0.3.48-x86_64-macos.tar.gz --uid=0 --gid=0 x86_64-macos
+tar czvf vosk-0.3.48-aarch64-macos.tar.gz --uid=0 --gid=0 aarch64-macos
 
-TAG='v0.1.0'
+TAG='v0.2.0'
 
-gh release create "$TAG" -n '
+gh release create "$TAG" -n "
 Builds of https://github.com/alphacep/vosk-api.
 
 ### NOTE
 
-This initial release is only for MacOS, and uses `-framework Accelerate`.
-'
+v0.3.48 isn't officially released yet.
 
-gh release upload --clobber "$TAG" vosk-0.3.45-x86_64-macos.tar.gz
-gh release upload --clobber "$TAG" vosk-0.3.45-aarch64-macos.tar.gz
+Vosk version: https://github.com/alphacep/vosk-api/tree/40937b6bcbe318eeb01879093c59cf5a1219a29d
+"
+
+gh release upload --clobber "$TAG" vosk-0.3.48-x86_64-macos.tar.gz
+gh release upload --clobber "$TAG" vosk-0.3.48-aarch64-macos.tar.gz
